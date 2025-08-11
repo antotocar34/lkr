561992496cd546d4f04e94d9d9c0e3626ce70a87
Antoine Carnec <antoinecarnec@gmail.com>
2025-08-11 09:59:38 +0100

    Added support for all systems

--- flake.nix
+++ flake.nix
@@ -2,18 +2,19 @@
   description = "lkr build flake";
 
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs }: let
-    pkgs = import nixpkgs {
-      system = "x86_64-linux";
-      overlays = [];
-    };
-  in {
-
-
-    packages.x86_64-linux.lkr = import ./default.nix { inherit pkgs; };
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.lkr;
-
-  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [];
+        };
+      in
+      {
+        packages.lkr = import ./default.nix { inherit pkgs; };
+        packages.default = self.packages.${system}.lkr;
+      }
+    );
 }
